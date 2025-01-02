5fc17faa4079bd036c7a81591ce541d6c1cb9a1b
SharlaVidal <144283288+SharlaVidal@users.noreply.github.com>
2025-01-02 17:15:23 -0300

    ajuste route

--- src/app/api/pets/route.ts
+++ src/app/api/pets/route.ts
@@ -1,18 +1,19 @@
 import prisma from "@/app/lib/prisma";
+import { NextResponse } from "next/server";
 
+// Lidar com o método POST
+export async function POST(req: Request) {
+  try {
+    const { name, ageCategory, size, description, photoUrl } = await req.json();
+    
+    // Cria um novo post no banco de dados
+    const newPost = await prisma.post.create({
+      data: { name, ageCategory, size, description, photoUrl },
+    });
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { name, ageCategory, size, description, photoUrl } = req.body;
-    try {
-      const newPost = await prisma.post.create({
-        data: { name, ageCategory, size, description, photoUrl },
-      });
-      res.status(201).json(newPost);
-    } catch (error) {
-      res.status(500).json({ error: "Erro ao criar o post." });
-    }
-  } else {
-    res.status(405).json({ error: "Método não permitido." });
+    // Retorna o post criado com status 201
+    return NextResponse.json(newPost, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: "Erro ao criar o post." }, { status: 500 });
   }
 }
